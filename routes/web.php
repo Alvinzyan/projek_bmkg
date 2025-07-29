4a9e39006117ad0b758debdf0ee348a710790170
Alvindra Ibnu <151527316+Alvinzyan@users.noreply.github.com>
2025-07-30 00:06:23 +0700

    integrasi tempalte

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+
+Route::get('/inventaris-alat', function () {
+    return view('inventaris-alat.index');
 });
